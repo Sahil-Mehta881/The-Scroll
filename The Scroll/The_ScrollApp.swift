1655f4e8a9562b9e8bf27feeb0c07fa01a7f9c39
Sahil Mehta <mehtasahil881@berkeley.edu>
2022-10-16 03:34:58 -0700

    Initial Commit

---
+++ The Scroll/The_ScrollApp.swift
@@ -0,0 +1,17 @@
+//
+//  The_ScrollApp.swift
+//  The Scroll
+//
+//  Created by Sahil Mehta on 10/16/22.
+//
+
+import SwiftUI
+
+@main
+struct The_ScrollApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
